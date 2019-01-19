1b7eb6245269df5df08d6f24f75507be19de9d1e
onemoon <onemoonstudio@gmail.com>
2019-01-19 12:43:23 +0900

    Refactor: response status
    Refactor
    서버의 응답을 이렇게 세분화 하는것이 혹여나 문제가 생겼을때 빠르게 파악하고 해결하기에 더 좋아보입니다!

--- MovieBoxOffice/Model/Network/StatueCodeResponse.swift
+++ MovieBoxOffice/Model/Network/StatueCodeResponse.swift
@@ -10,6 +10,8 @@ import Foundation
 
 enum ResponseResult{
     case success
+    case badRequest
+    case serverError
     case failure
 }
 
@@ -17,6 +19,8 @@ struct StatueCodeResponse {
     func result(_ response: HTTPURLResponse) -> ResponseResult{
         switch response.statusCode {
         case 200: return .success
+        case 400..<500: return .badRequest
+        case 500..<600: return .serverError
         default: return .failure
         }
     }
